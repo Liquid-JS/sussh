c13831ffa84d8780c15e23311b1d57ae5198e756
HitkoDev <development@hitko.si>
2021-05-03 19:26:28 +0200

    add volumes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -12,4 +12,16 @@ if [ ! -f "/usr/local/etc/auth_keys.ini" ]; then
     dockerize -template /usr/local/etc/auth_keys.ini.tpl:/usr/local/etc/auth_keys.ini
 fi
 
+if [ ! -f "/sussh/host-keys/ssh_host_rsa_key" ]; then
+    ssh-keygen -q -N "" -t rsa -b 4096 -f /sussh/host-keys/ssh_host_rsa_key
+fi
+
+if [ ! -f "/sussh/host-keys/ssh_host_ecdsa_key" ]; then
+    ssh-keygen -q -N "" -t ecdsa -f /sussh/host-keys/ssh_host_ecdsa_key
+fi
+
+if [ ! -f "/sussh/host-keys/ssh_host_ed25519_key" ]; then
+    ssh-keygen -q -N "" -t ed25519 -f /sussh/host-keys/ssh_host_ed25519_key
+fi
+
 /usr/sbin/sshd -D
